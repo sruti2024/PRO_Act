60fa3969e3d64b85056b3f6ec6ea9c98370d24fd
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2021-03-10 00:05:23 +0530

    Added a new field of pid in the project model which represents project id

--- home/models.py
+++ home/models.py
@@ -2,11 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Project_add(models.Model):
-    name=models.CharField(max_length=200)
-    desc=models.CharField(max_length=700)
-    link=models.CharField(max_length=200)
-    stack=models.CharField(max_length=300)
+    pid = models.CharField(max_length=50, null=True, blank=True)
+    name = models.CharField(max_length=200)
+    desc = models.CharField(max_length=700)
+    link = models.CharField(max_length=200)
+    stack = models.CharField(max_length=300)
     date = models.CharField(max_length=100)
+
+    def save(self, *args, **kwargs):
+        self.pid = str(str(self.name[0:3])+'_'+str(self.date).split('/')[0])
+        super(Project_add, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
