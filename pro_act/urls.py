ee468f717a265ee6e610fe75f0d2c2c324b693b6
Ashutosh Krishna <ashutoshbritish@gmail.com>
2021-03-11 11:55:28 +0530

    Add Static Root, Email Backend

--- pro_act/urls.py
+++ pro_act/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
 
 ]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
