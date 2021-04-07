78b656d0b938d580a7a401180e824708e21ce085
niksm7 <nikhilsmankani@gmail.com>
2021-04-07 20:50:53 +0530

    added subtasks feature

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Project_add, OTPModel,Profile
+from .models import Project_add, OTPModel,Profile,Subtask
 
 # Register your models here.
 admin.site.register(Project_add)
 admin.site.register(OTPModel)
 admin.site.register(Profile)
+admin.site.register(Subtask)
