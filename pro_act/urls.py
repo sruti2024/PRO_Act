d903a93779f87090799b680265c455cd45571b07
Aryamanz29 <aryamanz29@gmail.com>
2021-04-26 23:01:52 +0530

    chore : Lint & Format closes #154

--- pro_act/urls.py
+++ pro_act/urls.py
@@ -1,16 +1,14 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from home import views
 
 urlpatterns = [
     # path('', TemplateView.as_view(template_name=views.index)),
-    path('admin/', admin.site.urls),
-    path('',include('home.urls')),
-    path('accounts/', include('allauth.urls')),
-
+    path("admin/", admin.site.urls),
+    path("", include("home.urls")),
+    path("accounts/", include("allauth.urls")),
 ]
 
 if settings.DEBUG:
-	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
