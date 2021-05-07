8c0a246b5baf3e347ea2a003c3f60dc708c3c0ea
Aryamanz29 <aryamanz29@gmail.com>
2021-05-07 13:52:43 +0530

    Fix : (Add & Update) user profile picture

--- home/signals.py
+++ home/signals.py
@@ -1,22 +1,15 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+from .models import Profile
 
 
-@receiver(post_save, sender=User, dispatch_uid="save_new_user_profile")
-def save_profile(sender, instance, created, **kwargs):
-    user = instance
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
     if created:
-        profile = UserProfile(user=user)  # noqa
-        profile.save()
+        Profile.objects.create(user=instance)
 
 
-# @receiver(post_save, sender=User)
-# def create_profile(sender,instance,created,**kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
-
-
-# @receiver(post_save, sender=User)
-# def save_profile(sender,instance,**kwargs):
-#     instance.profile.save()
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
