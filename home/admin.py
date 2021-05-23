02c534b861979dbd7645cd5f962b6b48d6e64700
Aryamanz29 <aryamanz29@gmail.com>
2021-05-24 00:40:51 +0530

    [clean] : Remove Redundant Comments & Format

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Project_add, OTPModel, Profile, Subtask
 
-# Register your models here.
+
 admin.site.register(Project_add)
 admin.site.register(OTPModel)
 admin.site.register(Profile)
--- home/templatetags/format_datetime.py
+++ home/templatetags/format_datetime.py
@@ -6,6 +6,7 @@ register = template.Library()
 
 @register.filter
 def format_datetime(date):
+
     # This is a custom filter which format datetime string
     date = datetime.strptime(date, "%Y-%m-%d %H:%M:%S.%f")
     date = date.strftime("%d/%m/%Y %H:%M:%S")
